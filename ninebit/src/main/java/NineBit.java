c1d22a0ad827f73f9bba82af84ed85e0ead3b2f3
Bernd Dammer <Bernd.Dammer@web.de>
2026-02-01 20:14:26 +0100

    feierabend

--- ninebit/src/main/java/NineBit.java
+++ ninebit/src/main/java/NineBit.java
@@ -1,20 +1,20 @@
 class NineBit //
 
 {
-    private static final int POW2_DATA = 8;
-    private static final int POW2_CODE = 9;
-    private static final int COUNT_DATA = 256;
-    private static final int COUNT_CODE = 512;
+    private static final int POW2_DATA = 4;
+    private static final int POW2_CODE = 5;
+    private static final int COUNT_DATA = 1 << POW2_DATA;
+    private static final int COUNT_CODE = 1 << POW2_CODE;
+    private static final int TRIGGER = 10;
 
     private class Alphabet //
     {
-        private boolean[] b = new boolean[512];
-        private int[] index = new int[256];
+        private boolean[] b = new boolean[COUNT_CODE];
+        private int[] index = new int[COUNT_DATA];
 
         Alphabet() {
-            for (int i = 0; i < 256; i++) {
-                b[i] = true;
-                b[i + 256] = false;
+            for (int i = 0; i < COUNT_CODE; i++) {
+                b[i] = i < COUNT_DATA;
             }
             buildIndex();
         }
@@ -25,13 +25,13 @@ class NineBit //
 
         private void buildIndex() {
             int j = 0;
-            for (int i = 0; i < 512; i++) {
+            for (int i = 0; i < COUNT_CODE; i++) {
                 if (b[i]) {
                     index[j] = i;
                     j++;
                 }
             }
-            if (j != 256)
+            if (j != COUNT_DATA)
                 System.out.println("j " + j);
         }
 
@@ -39,7 +39,7 @@ class NineBit //
             int i;
             int lower_one_count = 0;
             boolean found = false;
-            for (i = 0; i < 512 - 1; i++) // search bit to shift up
+            for (i = 0; i < COUNT_CODE - 1; i++) // search bit to shift up
             {
                 if (b[i] && !b[i + 1]) // found one to shift up
                 {
@@ -67,10 +67,10 @@ class NineBit //
 
     private class CollectedAlphabet //
     {
-        boolean[] b = new boolean[512];
+        boolean[] b = new boolean[COUNT_CODE];
 
         CollectedAlphabet() {
-            for (int i = 0; i < 512; i++) {
+            for (int i = 0; i < COUNT_CODE; i++) {
                 b[i] = false;
             }
         }
@@ -81,11 +81,11 @@ class NineBit //
 
         public void pNotFound() {
             int c = 0;
-            for (int i = 0; i < 512; i++) {
+            for (int i = 0; i < COUNT_CODE; i++) {
                 if (!b[i])
                     c++;
             }
-            if (c > 3)
+            if (c > TRIGGER)
                 System.out.println("Number free bitmasks: " + c);
         }
     }
@@ -102,11 +102,11 @@ class NineBit //
     {
         CollectedAlphabet ca = new CollectedAlphabet();
         int i, j, k;
-        for (i = 0; i < 256 - 1; i++) {
-            for (j = i + 1; j < 256; j++) {
-                int mask = a.get(i) | a.get(j) << 8;
-                for (k = 0; k < 9; k++) {
-                    int ind = (mask >> k) & (512 - 1);
+        for (i = 0; i < COUNT_DATA - 1; i++) {
+            for (j = i + 1; j < COUNT_DATA; j++) {
+                int mask = a.get(i) | a.get(j) << POW2_CODE;
+                for (k = 0; k < POW2_CODE; k++) {
+                    int ind = (mask >> k) & (COUNT_CODE - 1);
                     ca.found(ind);
                 }
             }
